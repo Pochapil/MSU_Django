d6f767e48c9f3ed2e760512c939f4e2af4f7f956
Pochapil <bagrat37@mail.ru>
2022-05-14 01:13:56 +0300

    change models to restaurant

--- app/models.py
+++ app/models.py
@@ -16,15 +16,29 @@ class News(models.Model):
         verbose_name_plural = u"Новости"
 
 
-class Menu(models.Model):
-    price = models.IntegerField
-
-
 class RestaurantChain(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Название")
+    title = models.CharField(max_length=50, verbose_name="Название Сети")
+    menu = models.OneToOneField('Menu', verbose_name="Меню", on_delete=models.CASCADE)
     owners_name = models.CharField(max_length=50, verbose_name="Имя владельца")
-    menu = models.OneToOneField(Menu, on_delete=models.CASCADE, )
 
 
 class Restaurant(models.Model):
-    address = models.CharField(max_length=50, verbose_name="Адресс")
+    restaurant_chain = models.ForeignKey(RestaurantChain, verbose_name="Сеть ресторанов", on_delete=models.CASCADE)
+    address = models.CharField(max_length=50, verbose_name="Адрес")
+    manager_name = models.CharField(max_length=50, verbose_name="Имя менеджера")
+
+
+class Menu(models.Model):
+    dish = models.ManyToManyField('Dish', verbose_name="Блюдо")
+    serving_size = models.CharField(max_length=50, verbose_name="Размер порции")
+    price = models.IntegerField(verbose_name="Цена")
+
+
+class Dish(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название блюда")
+    dish_type = models.ForeignKey('DishTypes', verbose_name="Тип блюда", on_delete=models.CASCADE)
+    description = models.CharField(max_length=50, verbose_name="Описание блюда")
+
+
+class DishTypes(models.Model):
+    dish_type = models.CharField(max_length=50, verbose_name="Тип блюда")
