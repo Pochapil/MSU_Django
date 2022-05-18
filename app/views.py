7bbc8d21c405d3c093c60f9e675fee092419403f
Pochapil <bagrat37@mail.ru>
2022-05-19 01:14:22 +0300

    templates + url

--- app/views.py
+++ app/views.py
@@ -3,21 +3,8 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def year_archive(request, year):  # articles/<int:year>/
-    return HttpResponse("Hello, it's year!")
-
-
-def month_archive(request, year, month):  # articles/<int:year>/<int:month>/
-    return HttpResponse("Hello, it's month!")
-
-
-def article_detail(request, year, month, slug):  # articles/<int:year>/<int:month>/<slug:slug>/
-    return HttpResponse("Hello, it's details!")
-
-
-def index(request):  # /path
-    return HttpResponse("Hello, world. It’s index")
-
-
 def index(request):
     return render(request, 'app/index.html')
+
+def about(request):
+    return render(request, 'app/about.html')
--- prog2Sem2/urls.py
+++ prog2Sem2/urls.py
@@ -19,8 +19,5 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
-
-
-
+    path('', include('app.urls')),
 ]
