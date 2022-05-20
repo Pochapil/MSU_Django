0ae0bb5999040887df428ed9f2946bb430080c7e
Pochapil <bagrat37@mail.ru>
2022-05-21 00:53:40 +0300

    переобувка

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
-
 class RestaurantChain(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название Сети")
     menu = models.OneToOneField('Menu', verbose_name="Меню", on_delete=models.SET_NULL, null=True)
@@ -33,7 +32,7 @@ class Restaurant(models.Model):
 
 
 class Menu(models.Model):
-    dish = models.ManyToManyField('Dish', verbose_name="Блюдо")
+    dish = models.ForeignKey('Dish', verbose_name="Блюдо", on_delete=models.CASCADE)
     serving_size = models.CharField(max_length=50, verbose_name="Размер порции")
     price = models.IntegerField(verbose_name="Цена")
 
@@ -46,6 +45,7 @@ class Dish(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название блюда")
     dish_type = models.ForeignKey('DishType', verbose_name="Тип блюда", on_delete=models.SET_NULL, null=True)
     description = models.TextField(verbose_name="Описание блюда")
+    ingredient_list = models.ManyToManyField('Ingredient', verbose_name="Список ингредиентов")
 
     def __str__(self):
         return self.title
@@ -64,3 +64,14 @@ class DishType(models.Model):
     class Meta:
         verbose_name = u"Тип блюда"
         verbose_name_plural = u"Типы блюд"
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название ингредиента")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = u"Ингредиент"
+        verbose_name_plural = u"Ингредиенты"
